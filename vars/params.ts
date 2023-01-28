313ac4023e1d84bcd94ef0a565c53d7f88688ee2
Brad Brown <bradbrown@bradbrown.llc>
2023-01-28 00:19:57 -0500

    list of blockchain address pairs to send to ankr rpc

---
+++ vars/params.ts
@@ -0,0 +1,7 @@
+export const params = [
+    { blockchain: 'eth', contractAddress: '0x5af28eb9dec75e86cdfbd2e41a474b54b211c1c2' },
+    { blockchain: 'bsc', contractAddress: '0xd17584633bc8d190e5a14502976dad9640456d6d' },
+    { blockchain: 'fantom', contractAddress: '0xd17584633bc8d190e5a14502976dad9640456d6d' },
+    { blockchain: 'avalanche', contractAddress: '0xd17584633bc8d190e5a14502976dad9640456d6d' },
+    { blockchain: 'arbitrum', contractAddress: '0xa943F863fA69ff4F6D9022843Fb861BBEe45B2ce' }
+]
