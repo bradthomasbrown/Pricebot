cb34b144c1fb50d74fcb1d5f32ac880788f22100
Brad Brown <bradbrown@bradbrown.llc>
2023-01-28 00:21:22 -0500

    takesankr_getTokenPrice bare request template and params, returns pricebot output

---
+++ functions/getTanglePrices.ts
@@ -0,0 +1,11 @@
+import superagent from 'superagent'
+import { ankr_getTokenPrice, params } from '../vars/index.js'
+import { handleCatch, translate } from './index.js'
+
+export async function getTanglePrices() {
+    let reqs = params.map((params, id) => { return { id, ...ankr_getTokenPrice, params } })
+    let res = await superagent.post('https://rpc.ankr.com/multichain').send(reqs).catch(handleCatch)
+    return res['body'].map((res: { result: { blockchain: string; usdPrice: string } }) => `${translate(res.result.blockchain)}\t$${res.result.usdPrice.substring(0, 8)}`)
+        .join('\n')
+        .replace(/\./g, '\\.')
+}
